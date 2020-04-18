90a4bb44a42d46dd3fd58eb1f0212985275adaf3
springleo <782655835@qq.com>
2020-04-19 01:01:30 +0800

    feat: add vuele/elementui && remove eslint for src

--- src/app.js
+++ src/app.js
@@ -6,6 +6,31 @@ import { createRouter } from './router'
 import title from './mixins/title'
 
 Vue.mixin(title)
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI)
+
+import vuele from 'vuele';
+import 'vuele/dist/vuele.css';
+
+console.log(12312)
+Vue.use(vuele, {
+    // 传入项目级配置
+    selectUrl() {
+        return './test'; // 通用下拉url配置函数，需返回字符串类型
+    },
+    resolveCommonReturn(res) {
+        // 处理通用下拉接口的返回函数，更多使用可参见KSSelectMixin
+    },
+    remoteSelectUrl() {
+        return '/remote'; // 远程搜索下拉组件的通用url配置函数，需返回字符串类型
+    },
+    authUrl: '', // 系统级的获取权限接口，更多使用可参见KSAuthMixin文档
+    unauthorizedUrl: '', // 系统级的无权限页
+    transferAuthResult() {
+        // 权限接口返回值的处理函数，更多使用可参见KSAuthMixin文档
+    }
+})
 
 export function createApp() {
   const router = createRouter()
