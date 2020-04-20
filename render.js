41ad4907d3cb61286d62bb254c82c1c18d15b18e
springleo <782655835@qq.com>
2020-04-20 14:06:15 +0800

    test: node render

--- render.js
+++ render.js
@@ -18,10 +18,18 @@ const renderer = createBundleRenderer(serverBundle, {
   runInNewContext: false
 });
 
+let context = {
+  title: 'blog',
+  str: 'test', // https://ssr.vuejs.org/zh/api/#renderer-%E9%80%89%E9%A1%B9
+  styles: '<style>body { color: red }</style>',
+  state: {
+    name: 'springleo'
+  },
+  url: '/home', // 路由要正确
+}
 const render = function (context) {
   return new Promise((resolve, reject) => {
     renderer.renderToString(context, (err, html) => {
-      console.log(err, html, 9999)
       if (err) {
         reject(err)
       }
@@ -30,9 +38,22 @@ const render = function (context) {
   })
 }
 
-render({
-  title: 'blog',
-  url: '/home' // 路由要正确
+render(context).then(html => {
+  const initdata = `[{
+    id: 2,
+    name: 'ks-clipboard',
+    data: {
+      content: 'hale'
+    }
+  }]`
+  const initdataScript = `<script type="text/javascript">
+  window.INIT_DATA = ${initdata.replace(/\n+$/, '')};
+  </script>`
+
+  const scriptsString = context.renderScripts(); // https://ssr.vuejs.org/zh/guide/build-config.html#%E5%AE%A2%E6%88%B7%E7%AB%AF%E9%85%8D%E7%BD%AE-client-config
+  html = html.replace(scriptsString, `\n${initdataScript}\n${scriptsString}`);
+  fs.writeFileSync(path.join(__dirname, 'dist', 'index.html'), html, 'utf-8');
+  console.log('server side render success.');
 })
 
 process.on('unhandledRejection', error => {
